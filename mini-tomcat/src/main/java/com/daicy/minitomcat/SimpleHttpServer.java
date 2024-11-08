706f69a49c500b18d37cfab7a198b3eeae0a9a95
daichangya <daichangya@163.com>
2024-11-08 21:59:31 +0800

    第二章代码提交

--- mini-tomcat/src/main/java/com/daicy/minitomcat/SimpleHttpServer.java
+++ mini-tomcat/src/main/java/com/daicy/minitomcat/SimpleHttpServer.java
@@ -4,6 +4,10 @@ import com.google.common.io.Resources;
 import java.io.*;
 import java.net.*;
 
+/**
+ * @author 代长亚
+ * 一个简单的 HTTP 服务器，用于处理 GET 请求并返回静态文件。
+ */
 public class SimpleHttpServer {
     private static final int PORT = 8080;
     private static final String WEB_ROOT = "webroot"; // 静态文件根目录
